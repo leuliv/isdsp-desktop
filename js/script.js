7bad3d4eff129cc015152714b0be6880178c46ca
Leul Woldegabriel <leulwoldegabriel@gmail.com>
2019-11-22 11:12:09 +0300

    update 2

--- js/script.js
+++ js/script.js
@@ -1,34 +1,76 @@
-var app = angular.module('app',['ngRoute']);
-const {remote} = require('electron')
+var app = angular.module('app', ['ngRoute']);
+const { remote } = require('electron')
 
-app.config(function($routeProvider){
-	$routeProvider.when('/',{
-        //templateUrl: __dirname +'/components/auth/signup.html',
-		templateUrl: __dirname +'/components/home/home.html',
-		controller: 'homeCtrl'
-	})
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: __dirname + '/components/home/home.html',
+            controller: 'homeCtrl',
+            resolve: {
+                currentAuth: function (AuthService) {
+                    return AuthService.authenticate();
+                }
+            }
+        })
+        .when('/profile', {
+            templateUrl: __dirname + '/components/profile/profile.html',
+            controller: 'homeCtrl',
+            resolve: {
+                currentAuth: function (AuthService) {
+                    return AuthService.authenticate();
+                }
+            }
+        })
+        .when('/auth', {
+            templateUrl: __dirname + '/components/auth/auth.html'
+        })
+        .when('/signin', {
+            templateUrl: __dirname + '/components/auth/signin.html'
+        })
+        .when('/signup', {
+            templateUrl: __dirname + '/components/auth/signup.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+}
+]);
+
+app.run(['$rootScope', '$location',
+    function ($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function (event, next, previous, error) {
+            if (error == 'AUTH_REQUIRED') {
+                $rootScope.message = 'Sorry, you must log in to access that page';
+                $location.path('/auth');
+            } //AUTH_REQUIRED
+        }); // $routeChangeError
+    }
+]);
+
+app.factory('AuthService', function ($q) {
+    return {
+        authenticate: function () {
+            // Your authenication logic
+            return $q.reject('AUTH_REQUIRED');
+        }
+    }
 });
 
-app.config(function($routeProvider){
-	$routeProvider.when('/profile',{
-		templateUrl: __dirname +'/components/profile/profile.html',
-		controller: 'homeCtrl'
-	})
-});
 
-app.controller('headCtrl', function($scope) {
+app.controller('headCtrl', function ($scope) {
     var win = remote.getCurrentWindow()
-    $scope.close = function(){
+    $scope.close = function () {
         win.close()
     };
-    $scope.minimize = function(){
+    $scope.minimize = function () {
         win.minimize()
     };
-    $scope.maximize = function(){
+    $scope.maximize = function () {
         win.isMaximized() ? win.unmaximize() : win.maximize()
     };
 });
 
-app.controller('homeCtrl', function($scope) {
+app.controller('homeCtrl', function ($scope) {
 
 });
